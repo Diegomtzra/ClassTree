19d99e5e8960c4b8154dff60aa2eb4c7749a3c58
Tel36071486 <diego.mtz.ra@gmail.com>
2022-06-01 21:34:11 -0500

    Commit 2

--- Three.cpp
+++ Three.cpp
@@ -1,9 +1,42 @@
-// Three.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
+
+struct Nodo {
+    int dato;
+    Nodo* der;
+    Nodo* izq;
+};
+
+Nodo* addNodo(int);
+void insertNodo(Nodo*&, int);
+Nodo* arbol = NULL;
 
 int main()
 {
     std::cout << "Hello World!\n";
+}
+
+Nodo* addNodo(int x) {
+    Nodo* nuevo_nodo = new Nodo();
+    nuevo_nodo->dato = x;
+    nuevo_nodo->der = NULL;
+    nuevo_nodo->izq = NULL;
+
+    return nuevo_nodo;
+}
+
+void insertNodo(Nodo*& arbol, int x) {
+    if (arbol == NULL) {
+        Nodo* nuevo_nodo = addNodo(x);
+        arbol = nuevo_nodo;
+    }
+    else {
+        int valorRaiz = arbol->dato;
+        if (x < valorRaiz) {
+            insertNodo(arbol->izq, x);
+        }
+        else {
+            insertNodo(arbol->der, x);
+        }
+    }
 }
